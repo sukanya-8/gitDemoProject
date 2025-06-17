69cc20b51e770ff176a8806e534102afa6fc9666
sukanya <sukanyakaricheti1993@gmail.com>
2025-06-17 11:26:08 +0530

    Second Commit on gitDemoProject1...

--- gitDemoProject1/src/gitDemoProject1/GitDemoProgram.java
+++ gitDemoProject1/src/gitDemoProject1/GitDemoProgram.java
@@ -3,7 +3,7 @@ package gitDemoProject1;
 public class GitDemoProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git Project");
+		System.out.println("Hello gitDemoProject1");
 
 	}
 
